28b5524aa6df13de12c437c47bd7aa0efdb484e9
Taylor Pubins <tpubz@icloud.com>
2023-09-07 15:29:09 -0700

    [Board] Add Board class with initialize and display methods

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+class Board
+  attr_reader :board
+
+  def initialize(rows = 6, columns = 7)
+    @rows = rows
+    @columns = columns
+    # Initialize an empty board filled with '.'
+    @board = Array.new(rows) { Array.new(columns, '.') }
+  end
+
+  def display
+    # Loop through each row and print it
+    @board.each do |row|
+      puts row.join(' ')
+    end
+    # Print column numbers for easier reference
+    puts %w{A B C D E F G H I J K L M}[0...@columns].join(' ')
+  end
+end
