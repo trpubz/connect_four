029575db38388c5e9be9ff4dc9705c43bbc85114
Taylor Pubins <tpubz@icloud.com>
2023-09-08 15:42:47 -0700

    Change error message for invalid selection in MSG module to include the actual input value

--- lib/msg.rb
+++ lib/msg.rb
@@ -7,11 +7,13 @@ module MSG
 
   BYE_MSG = "I hate goodbyes, #{ENV["USER"]}!! Hope you'll play Connect Four again soon!"
 
-  X_OR_O_MSG = "Pick your piece! X or O??"
-
   P_OR_Q_ERR_MSG = "Sorry, I didn't understand that selection. press 'p' to play; 'q' to quit:"
 
-  SELECT_PIECE_ERR_MSG = "Sorry, I didn't understand that selection."
+  PLAYER_TURN_MSG = "Choose where you want to drop your token"
 
-  PLAYER_TURN_MSG = "Ok, "
+  def INPUT_ERR_MSG(input)
+    "#{input} is an invalid selection"
+  end
+
+  # X_OR_O_MSG = "Pick your piece! X or O??"
 end
