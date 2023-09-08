5f58e9eff5bbb72f8d386ccd63f1c41694f8d56a
Matt Darlington <matthewsdarlington@gmail.com>
2023-09-07 21:48:56 -0600

    let user #pick_piece for login session

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -1,5 +1,5 @@
-require 'board'
-require 'player'
+require './lib/board'
+require './lib/player'
 
 class Game_Engine
   attr_reader :player1,
@@ -8,7 +8,7 @@ class Game_Engine
               :board
 
   def initialize
-    @player1 = Player.new(ENV['USER'], "X")
+    @player1 = Player.new(ENV['USER'])
     @ai = Player.new("HAL", "O")
     @players = [@player1, @ai]
     @board = Board.new
@@ -16,10 +16,41 @@ class Game_Engine
 
   def start_game
     system("echo", WELCOME_MSG)
+    if gets.chomp.downcase == "q"
+      abort(BYE_MSG)
+    else
+      system("echo", PLAY_MSG)
+      if @player1.token == ""
+        pick_piece
+      end
+    end
   end
-
+  
+  def pick_piece
+    system("echo", X_OR_0_MSG)
+    player_token = gets.chomp
+      if player_token == "X" 
+        @player1.token = "X"
+      elsif player_token == "O"
+        @player1.token = "O"
+      else
+        system("echo", "Sorry, I didn't understand that selection...")
+        pick_piece
+      end
+    end
+  end
 
   WELCOME_MSG = "Let's play **** CONNECT FOUR ****
   Connect four of your checkers in a row while preventing your opponent from doing the same.\n
   press 'p' to play; 'q' to quit:"
+
+  PLAY_MSG = "#{ENV["USER"]} says we're playing Connect Four!!\n"
+  "Human's First!! Start the game by picking any letter on the board!"
+
+  BYE_MSG = "We hate goodbyes, #{ENV["USER"]}!! Hope you'll play Connect Four again soon!"
+
+  X_OR_O_MSG = "Pick your piece! X or O??"
 end
+
+game = Game_Engine.new
+game.start_game
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
 class Player
   attr_reader :name, :token
   
-  def initialize(name, token)
+  def initialize(name, token="")
     @name = name
     @token = token
   end
