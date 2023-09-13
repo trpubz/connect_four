78b0188e7f1996f0f2ad4e6b6a476cac9c97a3a4
Taylor Pubins <tpubz@icloud.com>
2023-09-13 14:26:56 -0700

    Refactor ConnectFour initialize method to remove unnecessary code, and fix an issue where `game_engine` was not being properly set

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,14 +1,90 @@
-require './lib/game_engine'
+# frozen_string_literal: true
+require_relative 'game_engine'
+require_relative 'multiplayer_game_engine'
+require_relative 'msg'
+require_relative 'ip_config'
 
 class ConnectFour
-  def initialize(game_engine)
-    @game_engine = game_engine
+  include MSG, IP_CONFIG
+
+  attr_reader :game_engine
+
+  def initialize
+    @game_engine = nil
+  end
+
+  def main_menu
+    puts WELCOME_MSG
+    play_quit = CLI.get_input
+    # play_quit = "P" # testing
+    if play_quit == "Q"
+      abort(BYE_MSG)
+    elsif play_quit == "P"
+      puts PLAY_MSG
+      set_game_engine
+    else
+      puts P_OR_Q_ERR_MSG
+      main_menu
+    end
+  end
+
+  #returns game engine MP or SP
+  def set_game_engine
+    puts ONEPLAYER_TWOPLAYER
+    game_mode = CLI.get_input
+    # game_mode = "2P" # testing
+    if game_mode == "1P"
+      @game_engine = GameEngine.new
+    elsif game_mode == "2P"
+      @game_engine = MPGameEngine.new
+    else
+      puts INPUT_ERR_MSG(game_mode)
+      set_game_engine
+    end
   end
 
   def start
-    @game_engine.main_menu
+    if @game_engine.class == MPGameEngine
+      self.loiter  # wait in lobby
+      @game_engine.play_game
+      self.main_menu
+    else
+      @game_engine.play_game
+      self.main_menu
+    end
+  end
+
+  def loiter
+    # make initial curl request
+    username = ENV['USER']
+    foe = ""
+    p1 = ""
+    # send /init request
+    response = `curl -s "#{P2P_IP}/init?player=#{username}"`.chomp # => '...patient'
+    until response == "start"
+      print WAITING_MSG
+      # send /start request; if game ready to start, will return foe
+      response = `curl -s "#{P2P_IP}/start?player=#{username}"`.chomp
+      if response.include?("start")
+        start_responses = response.split(" ")
+        foe = start_responses[1]
+        p1 = start_responses[2]  # p1 used to set @current_player
+        response = "start"
+        break
+      end
+      LOADER_MSG(7)  # wait 7 seconds
+    end
+
+    puts ""
+    puts OPP_FOUND
+    @game_engine.player1 = Player.new(username, token = "X")
+    @game_engine.player2 = Player.new(foe, token = "O")
+    @game_engine.set_current_player(p1)
+    print "game ready to start"
+    LOADER_MSG(3)  # wait 3 seconds
   end
 end
 
-game_session = ConnectFour.new
-game_session.start
+game = ConnectFour.new
+game.main_menu
+game.start
---
+++ lib/multiplayer_game_engine.rb
@@ -0,0 +1,147 @@
+require_relative 'board'
+require_relative 'player'
+require_relative 'msg'
+require_relative 'cli'
+require_relative 'ip_config'
+require 'thread'
+
+class MPGameEngine
+  include MSG, IP_CONFIG
+
+  attr_reader :board
+  attr_accessor :player1,
+                :player2,
+                :current_player
+
+  def initialize
+    @player1 = nil
+    @player2 = nil
+    @current_player = nil
+    @board = Board.new
+  end
+
+  def players
+    [@player1, @player2]
+  end
+
+  def set_current_player(p1)
+    @current_player = self.players.filter { |player| player.name == p1 }.first
+  end
+
+  def play_game  # Player has hit 'p'
+    game_over = false
+    until game_over
+      puts @board.display
+      turn_over = false
+      plyr = whose_turn
+      token_x, token_y = nil, nil # returned from #drop_token and passed to #win_condition
+      # local client
+      if plyr == @player1
+        input_received = false
+        column = nil
+        mutex = Mutex.new
+
+        input_thread = Thread.new do
+          puts PLAYER_TURN_MSG
+          # keep false until valid input => in-range, un-filled column
+          until turn_over
+            ready = IO.select([STDIN], nil, nil, 1.6)  # 4th arg is 'release after time' (s)
+
+            if ready
+              column = STDIN.gets.chomp.upcase
+              if valid_input(column)
+                mutex.synchronize do
+                  input_received = true
+                  turn_over = true
+                  column = column
+                end
+              else
+                puts INPUT_ERR_MSG(column)
+                # continue to run until local client enters valid input
+                # loops back to until turn_over
+              end
+            end
+          end
+        end
+
+        timer_thread = Thread.new do
+          10.times do |i|
+            print "#{10 - i }. . "
+            sleep 1.6
+            break if mutex.synchronize { input_received }
+          end
+          # if the player doesn't select a column, random valid selection will occur
+          until turn_over
+            column = Board::COLUMNS.sample
+            if valid_input(column)
+              puts TIMES_UP_MSG(column)
+              mutex.synchronize do
+                input_received = true
+                turn_over = true
+                column = column
+              end
+            end
+          end
+        end
+
+        [input_thread, timer_thread].each(&:join)
+
+        token_x, token_y = drop_token(column, plyr.token)
+        # send valid column selection to server
+        `curl -s "#{P2P_IP}/move?player=#{plyr.name}?column=#{column}"`
+      else  # remote client || @player2
+        until turn_over
+          11.times do
+            puts WAITING_FOR_PLYR_MSG(plyr.name)
+            response = `curl -s "#{P2P_IP}/status?player=#{@player1.name}"`.chomp
+            unless response.include?("patience")  # unless this fails => response == <valid letter>
+              token_x, token_y = drop_token(response, plyr.token)
+              turn_over = true
+              break
+            end
+            sleep 1.6
+          end
+        end
+      end
+
+      if @board.check_win(token_x, token_y, plyr.token)
+        puts @board.display
+        puts "#{VICTORY_MSG(plyr.name)}"
+        game_over = true
+      end
+
+      if @board.board_full?
+        puts @board.display
+        puts TIE_GAME_MSG
+        game_over = true
+      end
+    end
+    # send /reset command to server to clear out stored variables
+    `curl -s "#{P2P_IP}/reset"`
+  end
+
+  def valid_input(column)
+    idx = @board.column_to_index(column)
+    if Board::COLUMNS.include?(column) && @board.board[0][idx] == '.'
+      true
+    else
+      false
+    end
+  end
+
+  # returns player object and increments queue
+  def whose_turn
+    plyr = @current_player
+    if plyr == players[0]
+      @current_player = players[1]
+    else
+      @current_player = players[0]
+    end
+    plyr
+  end
+
+  def drop_token(column, token)
+    idx = @board.column_to_index(column)
+    @board.drop_token(column, token)
+  end
+end
